39b9348e2b87f93b47d7a6ca9dc868876225ab63
Josh Kline <github.com@track.jonfram.net>
2013-09-17 19:22:48 -0700

    Failing spec for File.realpath

--- spec/fakefs/file_spec.rb
+++ spec/fakefs/file_spec.rb
@@ -4,15 +4,15 @@ module FakeFS
   describe File do
     include FakeFS::SpecHelpers
 
+    let(:dir_one) { '/some-dir-one' }
+    let(:dir_two) { '/some-dir-two' }
+
+    before do
+      FileUtils.mkdir dir_one
+      FileUtils.mkdir dir_two
+    end
+
     context '.absolute_path' do
-      let(:dir_one) { '/some-dir-one' }
-      let(:dir_two) { '/some-dir-two' }
-
-      before do
-        FileUtils.mkdir dir_one
-        FileUtils.mkdir dir_two
-      end
-
       it 'one argument' do
         expect( File.absolute_path(dir_one) ).to eq dir_one
       end
@@ -29,6 +29,25 @@ module FakeFS
       end
       it 'dot dot relative two argument' do
         expect( File.absolute_path( File.join( '..', dir_one ), dir_two ) ).to eq dir_one
+      end
+    end
+
+    #
+    # The problem in all cases with FakeFS::File.realpath is that by
+    # delegating to RealFile.realpath it doesn't know about directories
+    # created in the fake file system.
+    #
+    context '.realpath' do
+      it 'one argument' do
+        expect( File.realpath(dir_one) ).to eq dir_one
+      end
+      it 'two arguments' do
+        expect( File.realpath( File.join( '..', dir_one ), dir_two ) ).to eq dir_one
+      end
+      it 'wtf' do
+        Dir.chdir dir_two
+        expect( Dir.pwd ).to eq dir_two
+        expect( File.realpath( File.join( '..', dir_one ) ) ).to eq dir_one
       end
     end
   end
